ec1e6801aef1a10dd8909a23377a27e5bfba6a82
Carlos Eduardo dos Santos Figueiredo <42427944+carloseduardonit@users.noreply.github.com>
2020-07-04 14:18:39 -0300

    Adição da aula 09: introdução de herança e polimofismo

---
+++ src/cursoBasicodeJava/aula09/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursoBasicodeJava.aula09;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Animal {
+
+    public void emititSom() {
+        System.out.println("");
+    }
+}
